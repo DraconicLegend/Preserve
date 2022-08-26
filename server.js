c1c3f9b9f7ed70fc7d6acbe52a862df4da5caf63
Andrew R <arebello91@gmail.com>
2022-08-26 19:11:43 -0400

    Before File rearrangement

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require("path");
 
 const app = express();
 app.listen(3000, () => console.log("Listening on Port 3000"));
+app.use(express.json({ limit: '1mb' }))
+
+
 
 app.get('/', (req, res) => {
   var options = {
@@ -22,29 +25,28 @@ app.get('/', (req, res) => {
   });
 })
 
-app.use(express.json({ limit: '1mb' }))
 
 
-app.post('/database', (req, res) => {
-  console.log(req.body);
-  database.insert({ name: req.body.input1, mood: req.body.input2 })
-  res.json({
-    status: "success",
-    name: req.body.input1,
-    mood: req.body.input2
-  });
-});
+// app.post('/database', (req, res) => {
+//   console.log(req.body);
+//   database.insert({ name: req.body.input1, mood: req.body.input2 })
+//   res.json({
+//     status: "success",
+//     name: req.body.input1,
+//     mood: req.body.input2
+//   });
+// });
 
-app.get('/alldata', (req, res) => {
-  database.find({}, (err, data) => {
-    if (err) {
-      response.end();
-      return;
-    }
-    console.log("Test");
-    res.send(data);
-  });
-})
+// app.get('/alldata', (req, res) => {
+//   database.find({}, (err, data) => {
+//     if (err) {
+//       response.end();
+//       return;
+//     }
+//     console.log("Test");
+//     res.send(data);
+//   });
+// })
 
 
 
