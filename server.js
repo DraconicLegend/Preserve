d9e9bd2a618b04955ca43e7753638b171a00ea8b
Andrew R <arebello91@gmail.com>
2022-08-26 21:57:54 -0400

    Fixed Server issue as well as routing most of the web pages

--- server.js
+++ server.js
@@ -7,18 +7,20 @@ const path = require("path");
 const app = express();
 app.listen(3000, () => console.log("Listening on Port 3000"));
 app.use(express.json({ limit: '1mb' }))
-
-
-
+app.use("/styles",  express.static(__dirname + '\\public\\styles'));
+app.use("/scripts", express.static(__dirname + '\\public\\scripts'));
+app.use("/images",  express.static(__dirname + '\\public\\styles\\images'));
+app.use("/images",express.static(__dirname+'\\public\\images'))
+//Home Directory
 app.get('/', (req, res) => {
   var options = {
     root: path.join(__dirname)
   };
 
-  var fileName = 'Donate.html';
+  var fileName = 'public/index.html';
   res.sendFile(fileName, options, function (err) {
     if (err) {
-      next(err);
+      console.log(err);
     } else {
       console.log('Sent:', fileName);
     }
@@ -27,26 +29,106 @@ app.get('/', (req, res) => {
 
 
 
-// app.post('/database', (req, res) => {
-//   console.log(req.body);
-//   database.insert({ name: req.body.input1, mood: req.body.input2 })
-//   res.json({
-//     status: "success",
-//     name: req.body.input1,
-//     mood: req.body.input2
-//   });
-// });
+//Home Directory
+app.get('/partners', (req, res) => {
+  var options = {
+    root: path.join(__dirname)
+  };
 
-// app.get('/alldata', (req, res) => {
-//   database.find({}, (err, data) => {
-//     if (err) {
-//       response.end();
-//       return;
-//     }
-//     console.log("Test");
-//     res.send(data);
-//   });
-// })
+  var fileName = 'public/Partners.html';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+})
+
+
+
+//Contact
+app.get('/contact', (req, res) => {
+  var options = {
+    root: path.join(__dirname)
+  };
+
+  var fileName = 'public/Contact.html';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+})
+
+//Success upon Contact form submit
+
+app.get('/success', (req, res) => {
+  var options = {
+    root: path.join(__dirname)
+  };
+
+  var fileName = 'public/Success.html';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+})
+
+
+
+
+//Donate Routing
+
+app.get('/donate', (req, res) => {
+  var options = {
+    root: path.join(__dirname)
+  };
+
+  var fileName = 'public/Donate.html';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+})
+
+
+
+
+
+
+//---------------------------------------BACKEND STUFF------------------------------------\\
+
+
+
+app.post('/database', (req, res) => {
+  console.log(req.body);
+  database.insert({ name: req.body.input1, mood: req.body.input2 })
+  res.json({
+    status: "success",
+    name: req.body.input1,
+    mood: req.body.input2
+  });
+});
+
+app.get('/alldata', (req, res) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    console.log("Test");
+    res.send(data);
+  });
+})
 
 
 
